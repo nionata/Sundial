4a8344b5be2b892b0cbb4c1a4774ac3b4fc6d006
ejuor <ejuor@users.noreply.github.com>
2017-01-21 16:39:17 +0800

    add site UI components

---
+++ src/components/ui/Footer.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="footer">
+        <div className="small-print">
+          <nav>
+            <ul>
+              <li><a href="//github.com/meerkats-sundial/sundial">GitHub</a></li>
+              <li><a href="#">Terms of Use</a></li>
+              <li><a href="#">Privacy Policy</a></li>
+              <li><a href="#">Contact Us</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/components/ui/NavBar.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import sundialLogo from '../../assets/images/favicon-32x32.png';
+
+class NavBar extends Component {
+  render() {
+    return (
+      <nav className="main-navigation">
+        <div>
+          <img src={sundialLogo} alt="meerkat icon" />
+          <a className="nav-home" href="#">Sundial</a>
+          <ul>
+            <li><a href="#features">Features</a></li>
+            <li><a href="//github.com/meerkats-sundial/sundial" target="_blank">Github</a></li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
