e73ac0911cd0962038f72d261218d40bd89b607e
ejuor <ejuor@users.noreply.github.com>
2017-01-21 16:40:59 +0800

    add comment to test landing page rendering

--- src/index.js
+++ src/index.js
@@ -10,3 +10,20 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+/* ==============================================
+ * Uncomment the code below and comment the code
+ * above to use Landing page. Code below
+ * is for testing only.
+ * ==============================================
+ */
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import LandingPage from './components/landingpage/index';
+// import './index.css';
+
+// ReactDOM.render(
+//   <LandingPage />,
+//   document.getElementById('root')
+// );
