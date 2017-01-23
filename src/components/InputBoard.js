d749fe6066dc53a52879d8bae32aa5c55e9ac8f6
Hòa Trần <hoatranhcmiu@gmail.com>
2017-01-23 17:25:06 +0700

    added 'add item' button and handleAddItem function

--- src/components/InputBoard.js
+++ src/components/InputBoard.js
@@ -1,6 +1,33 @@
 import React from "react";
 
 class InputBoard extends React.Component {
+    constructor(props) {
+        super(props)
+        this.renderInputForm = this.renderInputForm.bind(this)
+        this.handleAddItem = this.handleAddItem.bind(this)
+
+        this.state = {
+            toggleAddItem: true
+        }
+    }
+
+    renderInputForm() {
+        return (
+            <tr>
+                <td><input type="text" className="form-control" /></td>
+                <td><input type="text" className="form-control" /></td>
+                <td><input type="text" className="form-control" /></td>
+            </tr>
+        )
+    }
+
+    handleAddItem(e){
+        e.preventDefault()
+        this.setState({
+            toggleAddItem: !this.state.toggleAddItem
+        })
+    }
+
     render() {
         return (
             <div className="container">
@@ -23,13 +50,10 @@ class InputBoard extends React.Component {
                             <td>-2</td>
                             <td>2pm - 4pm</td>
                         </tr>
-                        <tr>
-                            <td>@musecode</td>
-                            <td>+5</td>
-                            <td>2pm - 4pm</td>
-                        </tr>
+                        {this.state.toggleAddItem ? this.renderInputForm() : null}
                     </tbody>
                 </table>
+                <a href="#" onClick={this.handleAddItem}>add item</a>
                 <button type="button" className="btn btn-default pull-right">Sundial!</button>
             </div>
         )
