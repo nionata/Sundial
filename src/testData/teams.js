cb09a28b0df19c50b1c4601a4b5aec58efa58621
Hòa Trần <hoatranhcmiu@gmail.com>
2017-01-24 11:01:05 +0700

    add testdata/teams.js

---
+++ src/testData/teams.js
@@ -0,0 +1,19 @@
+var teams = [
+        {
+            "name": "Meerkat Sundial",
+            "members": [
+                {
+                    "name": "musecode",
+                    "availTime": "5-10",
+                    "timezone": "+12"
+                },
+                {
+                    "name": "nionata",
+                    "availTime": "5-10",
+                    "timezone": "-5"
+                }
+            ]
+        }
+    ]
+
+export default teams
