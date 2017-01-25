600542d008d68292a6a91fe9672d9aef20a652e9
Hoa Tran <hoatranhcmiu@gmail.com>
2017-01-26 06:13:14 +0700

    fixed #generate-link css of landingpage

--- src/components/landingpage/index.js
+++ src/components/landingpage/index.js
@@ -14,7 +14,7 @@ class LandingPage extends Component {
     // smooth scrolling
     $(function() {
       $('a[href*="#"]:not([href="#"])').click(function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
           var target = $(this.hash);
           target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
           if (target.length) {
