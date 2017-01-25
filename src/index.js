0ae1a4f99263cefcdde1e6947e0f3545f9dcfec1
Hòa Trần <hoatranhcmiu@gmail.com>
2017-01-25 11:29:53 +0700

    add react-router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { Router, Route, browserHistory } from 'react-router'
 
 import App from './components/App'
 import store from './store'
@@ -10,10 +11,11 @@ import './index.css'
 
 
 ReactDOM.render(
-  <Provider store={ store }>
-    <App />
-  </Provider>
-  ,
+  <Provider store={store}>
+    <Router history={browserHistory} >
+      <Route path="/:sessionid" component={App} />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 )
 
