941b0910c3a21a405d2202bf21a873d70e0e5497
ejuor <ejuor@users.noreply.github.com>
2017-01-27 17:10:55 +0800

    restructure smooth scroll

--- src/components/landingpage/index.js
+++ src/components/landingpage/index.js
@@ -1,7 +1,5 @@
 import React, {Component} from 'react';
-
-import $ from 'jquery';
-
+import './smoothscroll';
 import './index.css';
 import NavBar from '../ui/NavBar';
 import Body from './body/index';
@@ -9,21 +7,6 @@ import Footer from '../ui/Footer';
 
 class LandingPage extends Component {
   render() {
-    // smooth scrolling
-    $(function() {
-      $('a[href*="#"]:not([href="#"])').click(function() {
-        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-          if (target.length) {
-            $('html, body').animate({
-              scrollTop: target.offset().top
-            }, 1000);
-            return false;
-          }
-        }
-      });
-    });
     return (
       <div>
         <NavBar />
