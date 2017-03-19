031abe7b23a0619c3409072632c37fe6c2453cfb
Nicholas Ionata <n.ionata129@gmail.com>
2017-03-19 13:41:38 -0400

    Removed store, added firebase, and updated routing

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux'
 import { Router, Route, browserHistory } from 'react-router'
 
 import App from './components/App'
-import store from './store'
 import LandingPage from './components/landingpage/index';
 
 //import database
@@ -25,13 +24,13 @@ import './index.css'
 const NotFound = () => (<h1>404.. This page is not found!</h1>)
 
 ReactDOM.render(
-  <Provider store={store}>
+  //<Provider store={store}>
     <Router history={browserHistory} >
       <Route path="/" component={LandingPage} />
       <Route path="/team/:id" component={App} />
       <Route path='*' component={NotFound} />
-    </Router>
-  </Provider>,
+    </Router>,
+  //</Provider>,
   document.getElementById('root')
 )
 
