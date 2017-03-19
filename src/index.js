c7e54192b9c99a73a5c670d45076efc7a643d410
Nicholas Ionata <n.ionata129@gmail.com>
2017-03-19 13:44:54 -0400

    Removed retro redux import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
 import { Router, Route, browserHistory } from 'react-router'
 
 import App from './components/App'
