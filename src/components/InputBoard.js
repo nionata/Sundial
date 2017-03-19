7b4751526e82db5ac48ea33a696acffee4c135f1
Nicholas Ionata <n.ionata129@gmail.com>
2017-03-19 13:41:19 -0400

    Added firebase, added state, added new button handling

--- src/components/InputBoard.js
+++ src/components/InputBoard.js
@@ -1,25 +1,57 @@
 import React from "react";
+import * as firebase from 'firebase';
+import ResultBoard from '../components/ResultBoard.js'
+import '../assets/styles/App.css'
 
 class InputBoard extends React.Component {
     constructor(props) {
         super(props)
         this.renderInputForm = this.renderInputForm.bind(this)
-        this.handleAddItem = this.handleAddItem.bind(this)
         this.renderMember = this.renderMember.bind(this)
+        this.renderResultBoard = this.renderResultBoard.bind(this)
+        this.handleAddItem = this.handleAddItem.bind(this)
         this.handleOnSave = this.handleOnSave.bind(this)
+        this.handleOnSundial = this.handleOnSundial.bind(this)
+
+        console.log(this.props.id)
 
         //local state
         this.state = {
-            isAddingItem: false //Booean value to enable or disable inputform
+            team: {
+                "name": "Meerkat Sundial",
+                "members": [
+                /*{
+                    "name": "@musecode",
+                    "availTimeStart": "5",
+                    "availTimeEnd": "10",
+                    "timezone": "+12"
+                },
+                {
+                    "name": "@nionata",
+                    "availTimeStart": "5",
+                    "availTimeEnd": "10",
+                    "timezone": "-5"
+                },
+                 {
+                    "name": "@ejour",
+                    "availTimeStart": "5",
+                    "availTimeEnd": "10",
+                    "timezone": "+8"
+                }*/
+                ]
+            },
+            isAddingItem: false, //Booean value to enable or disable inputform
+            isSundialActivated: false
         }
     }
 
     renderInputForm() {
         return (
             <tr>
-                <td><input type="text" ref="inputName" className="form-control" /></td>
-                <td><input type="text" ref="inputTimezone" className="form-control" /></td>
-                <td><input type="text" ref="inputAvailTime" className="form-control" /></td>
+                <td><input type="text" ref="inputName" className="form-control" placeholder="Full Name (ex. John Doe)"/></td>
+                <td><input type="text" ref="inputTimezone" className="form-control" placeholder="From GMT (ex. -5)"/></td>
+                <td><input type="text" ref="inputAvailTimeStart" className="form-control" placeholder="Start Time (ex. 1)"/></td>
+                <td><input type="text" ref="inputAvailTimeEnd" className="form-control" placeholder="End Time (ex. 5)"/></td>
             </tr>
         )
     }
@@ -29,8 +61,17 @@ class InputBoard extends React.Component {
             <tr key={index}>
                 <td>{item.name}</td>
                 <td>{item.timezone}</td>
-                <td>{item.availTime}</td>
+                <td>{item.availTimeStart}</td>
+                <td>{item.availTimeEnd}</td>
             </tr>
+        )
+    }
+
+    renderResultBoard() {
+        return (
+            <div className="container App-body">
+                <ResultBoard />
+            </div>
         )
     }
 
@@ -48,18 +89,62 @@ class InputBoard extends React.Component {
         })
 
         //collect user input
-        const {inputName, inputTimezone, inputAvailTime} = this.refs
+        const {inputName, inputTimezone, inputAvailTimeStart, inputAvailTimeEnd} = this.refs
 
         if (inputName.value !== "") {
             const inputMember = {
                 name: inputName.value,
                 timezone: inputTimezone.value,
-                availTime: inputAvailTime.value
+                availTimeStart: inputAvailTimeStart.value,
+                availTimeEnd: inputAvailTimeEnd.value
             }
-            //update redux state
-            this.props.addMember(inputMember)
+            this.state.team.members.push(inputMember)
         }
 
+        var offset = new Date().getTimezoneOffset()/60;
+        console.log(offset);
+
+        //console.log(this.state.team);
+
+    }
+
+    handleOnSundial(e) {
+        e.preventDefault()
+        this.setState({
+            isSundialActivated: !this.state.isSundialActivated
+        })
+    }
+
+    componentDidMount() {
+        const ref = firebase.database().ref().child("teams").child("-23523423422")
+        ref.on('value', snap => {
+            this.setState( {
+                team: {
+                "name": "Meerkat Sundial",
+                "id": "random String",
+                "members": [
+                /*{
+                    "name": "@musecode",
+                    "availTimeStart": "5",
+                    "availTimeEnd": "10",
+                    "timezone": "+12"
+                },
+                {
+                    "name": "@nionata",
+                    "availTimeStart": "5",
+                    "availTimeEnd": "10",
+                    "timezone": "-5"
+                },
+                 {
+                    "name": "@ejour",
+                    "availTimeStart": "5",
+                    "availTimeEnd": "10",
+                    "timezone": "+8"
+                }*/
+                ]
+            }
+            });
+        });
     }
 
     render() {
@@ -70,11 +155,12 @@ class InputBoard extends React.Component {
                         <tr>
                             <th>Name</th>
                             <th>Timezone</th>
-                            <th>Availability</th>
+                            <th>Availability Start</th>
+                            <th>Availability End</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {this.props.team.members.map(this.renderMember)}
+                        {this.state.team.members.map(this.renderMember)}
                         {this.state.isAddingItem ? this.renderInputForm() : null}
                     </tbody>
                 </table>
@@ -84,18 +170,14 @@ class InputBoard extends React.Component {
                     ) : (
                             <div>
                                 <a href="#" onClick={this.handleAddItem}>Add a teamate...</a>
-                                <button type="button" className="btn btn-default pull-right">Sundial!</button>
+                                <button type="button" className="btn btn-default pull-right" onClick={this.handleOnSundial}>Sundial!</button>
                             </div>
                         )
                 }
+                {this.state.isSundialActivated ? this.renderResultBoard() : null}
             </div>
         )
     }
-}
-
-InputBoard.PropTypes = {
-    team: React.PropTypes.object,
-    addMember: React.PropTypes.func
 }
 
 export default InputBoard;
