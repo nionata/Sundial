21db86f978ab0278aac9723d1699aa4010689b53
Nicholas Ionata <n.ionata129@gmail.com>
2017-03-20 12:54:16 -0400

    Updated header links

--- src/components/landingpage/body/Header.js
+++ src/components/landingpage/body/Header.js
@@ -5,7 +5,7 @@ class Header extends Component {
   render() {
     return (
       <header>
-        <h1 className='title'>sundial</h1>
+        <h1 className='title' id="title">Sundial</h1>
         <h2 className='sub-title'>
           Simple session based web app that will allow team members to input available times and their time zone and a time slot will be generated
         </h2>
