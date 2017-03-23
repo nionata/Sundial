75f1f0ee8a6252117721d49cc5a32de7eb7e6a2f
Nicholas Ionata <n.ionata129@gmail.com>
2017-03-23 19:14:11 -0400

    Edited heading

--- src/components/landingpage/body/Header.js
+++ src/components/landingpage/body/Header.js
@@ -7,7 +7,7 @@ class Header extends Component {
       <header>
         <h1 className='title' id="title">Sundial</h1>
         <h2 className='sub-title'>
-          Simple session based web app that will allow team members to input available times and their time zone and a time slot will be generated
+          Simple session based web app that allows remote teams to find meeting availbility across multiple time zones!
         </h2>
         <a href="#generate-link">Get Started</a>
       </header>
