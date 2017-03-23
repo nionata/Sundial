79f3cf74518b2c3148843ed28b0f5ad1e3c00032
Nicholas Ionata <n.ionata129@gmail.com>
2017-03-23 19:14:00 -0400

    Editing feature hightlight

--- src/components/landingpage/body/FeatureHighlight.js
+++ src/components/landingpage/body/FeatureHighlight.js
@@ -5,16 +5,18 @@ import './featurehighlight.css';
 class FeatureHighlight extends Component {
   render() {
     return (
-      <section>
-        <h2>Example usage</h2>
+      <section id="how-to">
+        <h2>How To</h2>
         <Grid className="feature-highlight" fluid>
           <Row>
             <Col xs={12} md={8}>
-              <img src="//placehold.it/350x150" alt="Feature highlight" className="responsive" />
+                <video id="video" width="719" autoplay>
+                    <source src="/src/assets/videos/firsthowto.mp4" type="video/mp4"/>
+                </video>
             </Col>
             <Col xs={12} md={4}>
               <h4 className="feature-caption">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a fringilla leo. Ut viverra nunc at ante consequat pulvinar.
+                Simply enter a name to get started. After that add yourself to the team. If you need to you can edit the team name or your in\formation.
               </h4>
             </Col>
           </Row>
@@ -26,7 +28,7 @@ class FeatureHighlight extends Component {
             </Col>
             <Col xs={12} md={4}>
               <h4 className="feature-caption">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a fringilla leo. Ut viverra nunc at ante consequat pulvinar.
+                Next copy the url and send it to your teamates. Have them enter their information and then hit sundial to check the team's availability.
               </h4>
             </Col>
           </Row>
