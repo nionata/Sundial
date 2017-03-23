674d925e01fd787460d0bcb469759d9edd5d324b
Nicholas Ionata <n.ionata129@gmail.com>
2017-03-23 19:14:48 -0400

    Edited footer

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -7,7 +7,7 @@ class Footer extends Component {
     return (
       <Navbar className="footer" inverse>
         <Nav pullRight>
-          <NavItem eventKey={3} href="//github.com/meerkats-sundial/sundial/">GitHub</NavItem>
+          <NavItem eventKey={3} href="https://github.com/nionata/Sundial" target="_blank">GitHub</NavItem>
           <NavItem eventKey={4} href="/terms">Terms of Use</NavItem>
           <NavItem eventKey={5} href="/privacy">Privacy Policy</NavItem>
           <NavItem eventKey={6} href="/contact">Contact Us</NavItem>
