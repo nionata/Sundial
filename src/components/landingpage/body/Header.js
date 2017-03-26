a11edcfed0783b0be982a7065c3f5b6ca95757db
Nicholas Ionata <n.ionata129@gmail.com>
2017-03-26 16:29:23 -0400

    Fixed spelling error

--- src/components/landingpage/body/Header.js
+++ src/components/landingpage/body/Header.js
@@ -7,7 +7,7 @@ class Header extends Component {
       <header>
         <h1 className='title' id="title">Sundial</h1>
         <h2 className='sub-title'>
-          Simple session based web app that allows remote teams to find meeting availbility across multiple time zones!
+          Simple session based web app that allows remote teams to find meeting availability across multiple time zones!
         </h2>
         <a href="#generate-link">Get Started</a>
       </header>
