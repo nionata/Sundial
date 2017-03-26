2d955bd44737ca87b7c473ba0d03e48a0348e443
Nicholas Ionata <n.ionata129@gmail.com>
2017-03-26 15:13:53 -0400

    Updated feature highlight

--- src/components/landingpage/body/FeatureHighlight.js
+++ src/components/landingpage/body/FeatureHighlight.js
@@ -1,22 +1,24 @@
 import React, {Component} from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
 import './featurehighlight.css';
+import firstVideo from '../../../assets/videos/tutOne.mp4';
+import secondVideo from '../../../assets/videos/tutTwo.mp4';
 
 class FeatureHighlight extends Component {
   render() {
     return (
       <section id="how-to">
-        <h2>How To</h2>
+        <h2>How It Works</h2>
         <Grid className="feature-highlight" fluid>
           <Row>
             <Col xs={12} md={8}>
-                <video id="video" width="719" autoplay>
-                    <source src="/src/assets/videos/firsthowto.mp4" type="video/mp4"/>
+                <video className="tut-video" width="100%" controls>
+                    <source src={firstVideo} type="video/mp4"/>
                 </video>
             </Col>
             <Col xs={12} md={4}>
               <h4 className="feature-caption">
-                Simply enter a name to get started. After that add yourself to the team. If you need to you can edit the team name or your in\formation.
+                Simply enter a name to get started. After that add yourself to the team. If you need to you can edit the team name or your information.
               </h4>
             </Col>
           </Row>
@@ -24,7 +26,9 @@ class FeatureHighlight extends Component {
         <Grid className="feature-highlight" fluid>
           <Row>
             <Col xs={12} md={8}>
-              <img src="//placehold.it/350x150" alt="Feature highlight" className="responsive" />
+                <video className="tut-video" width="100%" controls>
+                    <source src={secondVideo} type="video/mp4"/>
+                </video>
             </Col>
             <Col xs={12} md={4}>
               <h4 className="feature-caption">
