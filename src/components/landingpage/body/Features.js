ae609314689902c2f752b950fb7a598e35230e10
Nicholas Ionata <n.ionata129@gmail.com>
2017-03-26 21:03:01 -0400

    Updated feature links and changed grammar

--- src/components/landingpage/body/Features.js
+++ src/components/landingpage/body/Features.js
@@ -12,21 +12,21 @@ class Features extends Component {
             <p className="feature-description">
               Just enter a name and click a button to get started. From there just share the generated url with your teamates!
             </p>
-            <a className="button" href="//github.com/nionata/sundial" target="_blank">Learn more</a>
+            <a className="button" href="https://github.com/nionata/Sundial#getting-started" target="_blank">Learn more</a>
           </Col>
           <Col xs={12} sm={4} className="feature-box text-center">
             <h3 className="feature-title">No Complex Conversions</h3>
             <p className="feature-description">
-              You simply enter your availability in your time and let Sundial take care of finding your teams availability in your time!
+              You simply enter your availability, in your time, and let Sundial take care of finding your team's availability in your time!
             </p>
-            <a className="button" href="//github.com/nionata/sundial" target="_blank">Learn more</a>
+            <a className="button" href="https://github.com/nionata/Sundial#finding-availability" target="_blank">Learn more</a>
           </Col>
           <Col xs={12} sm={4} className="feature-box text-center">
             <h3 className="feature-title">Seemless Collaboration</h3>
             <p className="feature-description">
               As you make changes on one client, it automatically updates all the other open clients in real time!
             </p>
-            <a className="button" href="//github.com/nionata/sundial" target="_blank">Learn more</a>
+            <a className="button" href="https://github.com/nionata/Sundial#collaborating-with-your-team" target="_blank">Learn more</a>
           </Col>
         </Row>
       </Grid>
